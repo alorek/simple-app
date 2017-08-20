7129fa7bf724c74e4f16155722b781e1b678593d
Aleksandra Lorek <aleksandra.lorek@gmail.com>
2017-08-20 17:26:09 +0100

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { devToolsEnhancer } from 'redux-devtools-extension';
-import 'normalize.css/normalize.css';
-import './index.css';
-import App from './containers/App';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './index.css';
+
+import App from './containers/App';
 import RecordView from './components/RecordView';
 import reducer from './reducers';
 
